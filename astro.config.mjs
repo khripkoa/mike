9f3042fd952afc78f7930859efc789f4260704bf
Anton Khripko <khripkoa@gmail.com>
2026-01-17 15:07:37 -0500

    Update domain to handymikeclt.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://mike-handyman.com',
+    site: 'https://handymikeclt.com',
     output: 'static',
     trailingSlash: 'always',
     experimental: {
